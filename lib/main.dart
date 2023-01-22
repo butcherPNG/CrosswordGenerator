15db3d1fc54336b63d95ae59cc60b07795b1c5de
butcherPNG <iigor.isakov@yandex.ru>
2023-01-22 15:22:18 +0300

    1st version

--- lib/main.dart
+++ lib/main.dart
@@ -17,15 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
@@ -70,13 +62,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
             var row = MyDrawerHelper.getRowsInSquare(context, width, widget.c!);
 
-            // row.add(Positioned(
-            //     top: height / 2,
-            //     left: width / 2,
-            //     child: ElevatedButton(
-            //       child: Text("BTN"),
-            //       onPressed: () {},
-            //     )));
 
             return Scaffold(
               body: SingleChildScrollView(
